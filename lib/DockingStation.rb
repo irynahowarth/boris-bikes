a6d2e21859353fabca9b14d605d44287e3654eea
David Parry <drjparry@gmail.com>
2016-01-19 18:59:55 +0100

    added bike test

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,4 +1,10 @@
+require './lib/bike.rb'
+
+num_of_bikes = 60
+
 class DockingStation
   def release_bike
+    Bike.new
+
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,5 @@
+
+
 require 'DockingStation.rb'
 =begin ###
 describe DockingStation do
@@ -5,8 +7,24 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 =end
-
-
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
+
+=begin
+describe DockingStation do
+  it 'is working' do
+    bike = Bike.new
+    expect(bike).to be_working
+  end
+end
+
+=end
+
+
+
